63284fb251901c2f82c9d71110b2c7b5b38a6f7c
Anuj Yadav <anuj882711@gmail.com>
2026-03-15 12:44:32 +0530

    Middleware Add

--- InstaGram/src/controllers/post.controller.js
+++ InstaGram/src/controllers/post.controller.js
@@ -11,27 +11,9 @@ const imagekit = new ImageKit({
 })
 
 async function createPostController(req,res) {
-    console.log(req.body, req.file)
 
-    const token = req.cookies.token
 
-    if(!token){
-        return res.status(401).json({
-            message: "Token not provided, Unauthorized access"
-        })
-    }
-    
-    let decoded;
-
-    try{
-         decoded = jwt.verify(token, process.env.JWT_SECRET)
-    } catch (err) {
-        return res.status(401).json({
-            message: "User not authorized"
-        })
-    }
-
-    console.log(decoded)
+   
 
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer),'file'),
@@ -42,7 +24,7 @@ async function createPostController(req,res) {
     const post = await postModel.create({
         caption: req.body.caption,
         imgUrl : file.url,
-        user : decoded.id
+        user : req.user.id
     })
 
     res.status(201).json({
@@ -53,22 +35,9 @@ async function createPostController(req,res) {
 
 async function getPostController(req,res){
 
-    const token = req.cookies.token
+    
 
-    let decoded;
-
-    try{
-        const decoded =  jwt.verify(token, process.env.JWT_SECRET)
-    } 
-    catch(err){
-
-        return res.status(401).json({
-            message: "TOken invalid"
-
-        })
-    }
-
-    const userId = decoded.id
+    const userId = req.user.id
 
     const posts = await postModel.find({
         user: userId
@@ -85,28 +54,9 @@ async function getPostController(req,res){
 
 async function getPostDetailsController(req,res){
 
-    const token = req.cookies.token
+    
 
-    if(!token){
-        return res.status(401)({
-            message: "UnAuthorized Access"
-        })
-    }
-
-    let decoded;
-
-    try{
-       
-        decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-    }
-    catch(err){
-        return res.status(401).json({
-            message: "Invalid Token"
-        })
-    }
-
-    const userId = decoded.id
+    const userId = req.user.id
     const postId = req.params.postid
 
     const post = await postModel.findById(postId)
--- InstaGram/src/routes/post.routes.js
+++ InstaGram/src/routes/post.routes.js
@@ -3,27 +3,27 @@ const postRouter = express.Router()
 const postController = require("../controllers/post.controller")
 const multer = require("multer")
 const upload = multer({storage: multer.memoryStorage() })
-
+const identifyuser = require("../middlewares/auth.middleware")
 
 /**
  * Post / api/posts
  * - req.body = { caption , image-file }
  */
 
-postRouter.post("/",upload.single("image"),postController.createPostController)
+postRouter.post("/",upload.single("image"), identifyuser ,postController.createPostController)
 
 /**
  * GET/ api/posts/[protected]
  */
 
-postRouter.get("/", postController.getPostController)
+postRouter.get("/", identifyuser , postController.getPostController)
 
 /**
  * Get/api/posts/details/:postid
  * - return detail about specific post with the id, also check wether the post belongs to the user that the request come from
  */
 
-postRouter.get("/details/:postid",postController.getPostDetailsController )
+postRouter.get("/details/:postid", identifyuser ,postController.getPostDetailsController )
 
 
 
