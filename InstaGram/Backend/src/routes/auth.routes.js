d61b966cfaa20be6486bd6dfee43e23032ef39f8
Anuj Yadav <anuj882711@gmail.com>
2026-03-20 01:04:59 +0530

    Manage and Creating Layers in React

--- InstaGram/Backend/src/routes/auth.routes.js
+++ InstaGram/Backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const authController = require("../controllers/auth.controllers")
+const identifyuser = require("../middlewares/auth.middleware")
 
 const authRouter = express.Router()
 
@@ -13,5 +14,12 @@ authRouter.post('/register',authController.registerController )
  */
 
 authRouter.post("/login", authController.loginController)
+
+/**
+ * @route Get/api/auth/get-me
+ * @description Get the currently logged in user's information
+ * @access Private
+ */
+authRouter.get("/get-me", identifyuser , authController.getMeController)
 
 module.exports = authRouter
--- InstaGram/Frontend/src/App.jsx
+++ InstaGram/Frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { RouterProvider } from "react-router"
 import AppRoutes from "./AppRoutes"
 import "./style.scss"
+import { AuthProvider } from "./features/auth/auth.context.jsx"
 
 const App = () => {
   return (
-    <AppRoutes />
+    <AuthProvider>
+      <AppRoutes />
+      <h1>Welcome to the App</h1>
+    </AuthProvider>
   )
 }
 
